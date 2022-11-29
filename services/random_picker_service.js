e75c12a8e9a4e080ac41960e50fd2eb6db2b4c34
Pinchett-i <sebastien.pinchetti@blue-planet.be>
2022-11-29 14:55:28 +0100

    Add endpoint for modal open, load options, and submit
    At the moment, the views are placeholder static versions, but will
    be replaced once the data can be dynamically set

---
+++ services/random_picker_service.js
@@ -0,0 +1,25 @@
+class RandomPickerService {
+  // constructor(task, users) {
+  //   this.task = task
+  //   this.users = users
+  // }
+
+  pick_random(users, task) {
+    // filter through given users to find those with lowest count
+    let challengers = this.users_with_lowest_count_for_given_task(users, task)
+    // if more than one is found, randomize through those
+    let winner = this.get_one_random(challengers)
+    return winner
+  }
+
+  users_with_lowest_count_for_given_task(users, task) {
+    let user_tasks = UserTasks.where({ task: task, user: [users] })
+    user_tasks.reduce((accumulator, user_task) => {
+      accumulator.counter < user_task.counter ? accumulator : user_task
+    })
+  }
+
+  get_one_random(array) {
+    return array[Math.floor(Math.random() * array.length)]
+  }
+}
